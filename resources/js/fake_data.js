cab09d0754a45daf68689da9aa72f9de6df36aa5
Diana Boudy <diana.boudy@gmail.com>
2015-11-07 18:31:39 +0100

    creation des jisons

--- resources/js/fake_data.js
+++ resources/js/fake_data.js
@@ -1,22 +1,80 @@
 "use strict"
 
 window.fake_data = {
-	Tb42: [
+	"Tb42": [
+
 		{
-			name: "foo",
-			description: "blablabla",
-			icon: "foo.png",
-			amount: 4,
-			unit_price_times_100: 550
+			name: "Hamburger",
+			description: "Chevre moutarde et miel",
+			col: 0,
+			row: 0,
+			amount: 2,
+			unit_price_times_100: 890,
 		},
 		{
-			name: "bar",
-			description: "blablabla",
-			icon: "bar.png",
+			name: "Hot-dog",
+			description: "100% boeuf avec oignons frits",
+			col: 4,
+			row: 0,
 			amount: 2,
-			unit_price_times_100: 4200
-		}
-
+			unit_price_times_100: 650,
+		},
+		{
+			name: "Frites",
+			description: "faites maison avec du sel de guérande",
+			col: 2,
+			row: 0,
+			amount: 4,
+			unit_price_times_100: 300,
+		},
+		{
+			name: "Guiness",
+			description: "50 cl",
+			col: 3,
+			row: 1,
+			amount: 7,
+			unit_price_times_100: 750
+		},
+		{
+			name: "Bloody mary",
+			description: "Tomate, vodka et tabasco",
+			col: 1,
+			row: 1,
+			amount: 1,
+			unit_price_times_100: 850
+		},
+		{
+			name: "Café",
+			description: "100% arabica",
+			col: 1,
+			row: 1,
+			amount: 6,
+			unit_price_times_100: 200
+		},
+		{
+			name: "Macaron",
+			description: "Vendu par 4",
+			col: 7,
+			row: 3,
+			amount: 2,
+			unit_price_times_100: 890
+		},
+		{
+			name: "Donuts",
+			description: "Pour Omer",
+			col: 3,
+			row: 3,
+			amount: 1,
+			unit_price_times_100: 890
+		},
+		{
+			name: "Le Janis",
+			description: "Milkshake Oreo",
+			col: 6,
+			row: 1,
+			amount: 5,
+			unit_price_times_100: 600
+		},
 	]
 }
 
