1214150db53eab437f4ffc1694ac16e7d2e1e926
Diana Boudy <diana.boudy@gmail.com>
2015-11-08 13:08:08 +0100

    mise à jour du menu commandé et ajout logo 96 et 145

--- resources/js/fake_data.js
+++ resources/js/fake_data.js
@@ -4,36 +4,20 @@ window.fake_data = {
 	"Tb42": [
 
 		{
-			name: "Hamburger",
-			description: "Chevre moutarde et miel",
-			col: 0,
-			row: 0,
-			amount: 2,
-			unit_price: 8.90,
-		},
-		{
 			name: "Hot-dog",
-			description: "100% boeuf avec oignons frits",
+			description: "100% boeuf oignons",
 			col: 4,
 			row: 0,
-			amount: 2,
-			unit_price: 6.50,
+			amount: 4,
+			unit_price: 5.50,
 		},
 		{
 			name: "Frites",
-			description: "faites maison avec du sel de guérande",
+			description: "faites maison",
 			col: 2,
 			row: 0,
 			amount: 4,
 			unit_price: 3.00,
-		},
-		{
-			name: "Guiness",
-			description: "50 cl",
-			col: 3,
-			row: 1,
-			amount: 7,
-			unit_price: 7.50
 		},
 		{
 			name: "Bloody mary",
@@ -41,38 +25,38 @@ window.fake_data = {
 			col: 1,
 			row: 1,
 			amount: 1,
-			unit_price: 8.50
+			unit_price: 9.00
 		},
 		{
-			name: "Cafe",
-			description: "100% arabica",
-			col: 1,
+			name: "Guiness",
+			description: "50 cl",
+			col: 3,
 			row: 1,
-			amount: 6,
-			unit_price: 2.00
+			amount: 3,
+			unit_price: 7.50
+		},
+		{
+			name: "St-Emilion",
+			description: "Chateau Beauséjour 2006",
+			col: 2,
+			row: 1,
+			amount: 1,
+			unit_price: 84.00
 		},
 		{
 			name: "Macaron",
-			description: "Vendu par 4",
+			description: "Bannane et café",
 			col: 7,
 			row: 3,
-			amount: 2,
-			unit_price: 8.90
-		},
-		{
-			name: "Donuts",
-			description: "Pour Omer",
-			col: 3,
-			row: 3,
 			amount: 1,
-			unit_price: 8.90
+			unit_price: 6.00
 		},
 		{
 			name: "Le Janis",
 			description: "Milkshake Oreo",
 			col: 6,
 			row: 1,
-			amount: 5,
+			amount: 3,
 			unit_price: 6.00
 		},
 	]
